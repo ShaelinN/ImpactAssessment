a3fd8bd0a4e6f7e4dd1e5b5d8302f010e0c1f0d4
ShaelinN <shaelin100@gmail.com>
2023-04-06 21:39:23 +0200

    Allow user input through console in Main.java

--- app/src/main/java/impactassessment/Main.java
+++ app/src/main/java/impactassessment/Main.java
@@ -1,15 +1,26 @@
 package impactassessment;
 
 import java.util.Collection;
+import java.util.Scanner;
 
 import impactassessment.numberrangesummarizer.InputValidatingNumberRangeSummarizer;
-
+/**
+ * Note, this class simply demonstrates the inner functioning of the highest level class in the 
+ * numberrangesummarizer package (InputValidatingNumberRangeSummarizer).
+ * 
+ * In a 'real' use case, the wider application would need to implement its own checks to ensure 
+ * exceptions thrown by this class are handled in a way that suites the use case more appropriately.
+ */
 public class Main {
     public static void main(String[] args) {
         InputValidatingNumberRangeSummarizer ns = new InputValidatingNumberRangeSummarizer();
-        String input = "1,1,1,3,6,7,8,12,13,14,15,21,22,23,24,31"; 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide a comma separated list of integers (positive or negative, no spaces):");
+
+        String input = scanner.nextLine();
         Collection<Integer> col = ns.collect(input);
         String res = ns.summarizeCollection(col);
         System.out.println(res);
+        scanner.close();
     }
 }
